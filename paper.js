5d0b3581daaf41405434453c5a8b40fdcfd1bd74
AryanPadhye <73173945+AryanPadhye@users.noreply.github.com>
2020-11-18 16:01:52 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class paper {
+
+ constructor(x,y,r){
+     var options={
+         isStatic:false,
+         restitution:0.3,
+         friction:0.5,
+         density:1.2
+     }
+     this.x=x
+     this.y=y
+     this.r=r
+     this.body=Bodies.circle(this.x,this.y,this.r/2,options)
+     World.add(world,this.body);
+     
+    } 
+display(){
+    var paper=this.body.position;
+
+    push()
+    translate(paper.x,paper.y)
+    rectMode(CENTER)
+    strokeWeight(1)
+    fill("red")
+    ellipse(this.x,this.y,this.r,this.r)
+    pop()
+    }
+}
